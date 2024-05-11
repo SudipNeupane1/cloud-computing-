317661c62f7d318a38854a5887c0e4f54e6a0308
unknown <sudip.@neupane.com>
2024-05-11 03:27:59 -0400

    Books

---
+++ Lab/home.py
@@ -0,0 +1,14 @@
+# data types 
+# integer, string, tuples, dictionary, list.
+# control structure 
+#  functions 
+
+def home():
+    print("hello world")
+    
+   a =  input(Enter a number)
+
+   b = input(Enter b number);
+   c = a+b;
+
+print(home())
